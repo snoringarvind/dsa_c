a054a69e2493010f48a63fd744ecaaac7ea3caae
arvind <snoringarvind@gmail.com>
2022-08-30 11:29:29 +0530

    added dynamic array

--- Section9.c
+++ Section9.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 // for diagonal Matrix
 struct Matrix
 {
-    int A[15];
+    int *A;
     int n; // sizeofMatrix;
 };
 
@@ -17,23 +18,23 @@ void SetDiagonal(struct Matrix *m, int i, int j, int x)
 };
 
 // for diagonal Matrix
-int GetDiagonal(struct Matrix m, int i, int j)
+int GetDiagonal(struct Matrix *m, int i, int j)
 {
-    if (i == j && i < m.n)
-        return m.A[i];
+    if (i == j && i < m->n)
+        return m->A[i];
     else
         return 0;
 }
 
 // display it as 2D array
-void DisplayDiagonal(struct Matrix m)
+void DisplayDiagonal(struct Matrix *m)
 {
-    for (int i = 0; i < m.n; i++)
+    for (int i = 0; i < m->n; i++)
     {
-        for (int j = 0; j < m.n; j++)
+        for (int j = 0; j < m->n; j++)
         {
             if (i == j)
-                printf("%d ", m.A[i]);
+                printf("%d ", m->A[i]);
             else
                 printf("0 ");
         }
@@ -60,14 +61,14 @@ int GetLowerTriangularMatrix(struct Matrix *m, int i, int j)
         return 0;
 }
 
-void DisplayLowerTriangularMatrixUsingR(struct Matrix m)
+void DisplayLowerTriangularMatrixUsingR(struct Matrix *m)
 {
-    for (int i = 1; i <= m.n; i++)
+    for (int i = 1; i <= m->n; i++)
     {
-        for (int j = 1; j <= m.n; j++)
+        for (int j = 1; j <= m->n; j++)
         {
             if (i >= j)
-                printf("%d ", m.A[(i * (i - 1)) / 2 + (i - j)]);
+                printf("%d ", m->A[(i * (i - 1)) / 2 + (i - j)]);
             else
                 printf("%d ", 0);
         }
@@ -195,32 +196,141 @@ void DisplayUpperTriangularMatrixUsingC(struct Matrix *m)
     }
 }
 
+void SetTriDiagonalMatrix(struct Matrix *m, int i, int j, int x)
+{
+    i += 1;
+    j += 1;
+
+    if (i > m->n || j > m->n)
+        return;
+
+    if (i - j == 1)
+        m->A[i - 2] = x;
+    else if (i - j == 0)
+        m->A[m->n + 1 + i - 1] = x;
+    else if (i - j == -1)
+        m->A[2 * (m->n - 1) + i] = x;
+}
+
+int GetTriagonalMatrix(struct Matrix *m, int i, int j, int x)
+{
+
+    i += 1;
+    j += 1;
+
+    if (i - j == 1)
+        return m->A[i - 2];
+    else if (i - j == 0)
+        return m->A[m->n + 1 + i - 1];
+    else if (i - j == -1)
+        return m->A[2 * (m->n - 1) + i];
+    else
+        return 0;
+}
+
+void DisplayTriagonalMatrix(struct Matrix *m)
+{
+    for (int i = 1; i <= m->n; i++)
+    {
+        for (int j = 1; j <= m->n; j++)
+        {
+            if (i - j == 1)
+                printf("%d ", m->A[i - 2]);
+            else if (i - j == 0)
+                printf("%d ", m->A[m->n + 1 + i - 1]);
+            else if (i - j == -1)
+                printf("%d ", m->A[2 * (m->n - 1) + i]);
+            else
+                printf("%d ", 0);
+        }
+        printf("\n");
+    }
+}
+
+void SetToeplitxMatrix(struct Matrix *m, int i, int j, int x)
+{
+    i += 1;
+    j += 1;
+
+    if (i > m->n || j > m->n)
+        return;
+
+    if (i - j < 1)
+        m->A[j - i] = x;
+    else if (i - j >= 1)
+        m->A[m->n + i - j - 1] = x;
+}
+
+int GetToeplitxMatrix(struct Matrix *m, int i, int j)
+{
+    i += 1;
+    j += 1;
+
+    if (i - j < 1)
+        return m->A[j - i];
+    else if (i - j >= 1)
+        return m->A[m->n + i - j - 1];
+    else
+        return 0;
+}
+
+void DisplayToeplitxMatrix(struct Matrix *m)
+{
+    for (int i = 1; i <= m->n; i++)
+    {
+        for (int j = 1; j <= m->n; j++)
+        {
+            if (i - j < 1)
+                printf("%d ", m->A[j - i]);
+            else if (i - j >= 1)
+                printf("%d ", m->A[m->n + i - j - 1]);
+            else
+                printf("%d ", 0);
+        }
+        printf("\n");
+    }
+}
+
 int main()
 {
+    int matrixSize = 5;
+
     struct Matrix m;
-    m.n = 5;
+    m.n = matrixSize;
+    m.A = (int *)malloc(matrixSize * sizeof(int));
 
-    SetUpperTriangularMatrixUsingC(&m, 0, 0, 5);
-    SetUpperTriangularMatrixUsingC(&m, 0, 1, 6);
-    SetUpperTriangularMatrixUsingC(&m, 0, 2, 7);
-    SetUpperTriangularMatrixUsingC(&m, 0, 3, 9);
-    SetUpperTriangularMatrixUsingC(&m, 0, 4, 3);
+    SetDiagonal(&m, 0, 0, 1);
+    SetDiagonal(&m, 0, 1, 0);
+    SetDiagonal(&m, 0, 2, 0);
+    SetDiagonal(&m, 0, 3, 0);
+    SetDiagonal(&m, 0, 4, 0);
 
-    SetUpperTriangularMatrixUsingC(&m, 1, 1, 23);
-    SetUpperTriangularMatrixUsingC(&m, 1, 2, 1);
-    SetUpperTriangularMatrixUsingC(&m, 1, 3, 8);
-    SetUpperTriangularMatrixUsingC(&m, 1, 4, 4);
+    SetDiagonal(&m, 1, 0, 0);
+    SetDiagonal(&m, 1, 1, 1);
+    SetDiagonal(&m, 1, 2, 0);
+    SetDiagonal(&m, 1, 3, 0);
+    SetDiagonal(&m, 1, 4, 0);
 
-    SetUpperTriangularMatrixUsingC(&m, 2, 2, 7);
-    SetUpperTriangularMatrixUsingC(&m, 2, 3, 12);
-    SetUpperTriangularMatrixUsingC(&m, 2, 4, 15);
+    SetDiagonal(&m, 2, 0, 0);
+    SetDiagonal(&m, 2, 1, 0);
+    SetDiagonal(&m, 2, 2, 1);
+    SetDiagonal(&m, 2, 3, 0);
+    SetDiagonal(&m, 2, 4, 0);
 
-    SetUpperTriangularMatrixUsingC(&m, 3, 3, 25);
-    SetUpperTriangularMatrixUsingC(&m, 3, 4, 35);
+    SetDiagonal(&m, 3, 0, 0);
+    SetDiagonal(&m, 3, 1, 0);
+    SetDiagonal(&m, 3, 2, 0);
+    SetDiagonal(&m, 3, 3, 1);
+    SetDiagonal(&m, 3, 4, 0);
 
-    SetUpperTriangularMatrixUsingC(&m, 4, 4, 15);
+    SetDiagonal(&m, 4, 0, 0);
+    SetDiagonal(&m, 4, 1, 0);
+    SetDiagonal(&m, 4, 2, 0);
+    SetDiagonal(&m, 4, 3, 0);
+    SetDiagonal(&m, 4, 4, 1);
 
-    DisplayUpperTriangularMatrixUsingC(&m);
+    DisplayDiagonal(&m);
 
+    free(m.A);
     return 0;
 }
